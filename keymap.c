3760d5007cf361b135224921f6557caf1b46770f
Tom Kunzemann <tom.kunzemann@smartplatz.de>
2018-12-20 21:37:53 +0100

    use key_no

--- keymap.c
+++ keymap.c
@@ -27,87 +27,87 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_ergodox(
         // left hand
-        KC_AUDIO_VOL_DOWN,  KC_1,           KC_2,             KC_3,         KC_4,     KC_5,   LT(NPAD,KC_TRNS),
-        KC_DELT,            KC_Q,           KC_W,             KC_E,         KC_R,     KC_T,   KC_TRNS,
+        KC_AUDIO_VOL_DOWN,  KC_1,           KC_2,             KC_3,         KC_4,     KC_5,   LT(NPAD,KC_NO),
+        KC_DELT,            KC_Q,           KC_W,             KC_E,         KC_R,     KC_T,   KC_NO,
         LT(SYMB,KC_BSPC),   LT(MDIA, KC_A), KC_S,             KC_D,         KC_F,     KC_G,
         KC_LSFT,            CTL_T(KC_Y),    KC_X,             KC_C,         KC_V,     KC_B,   ALL_T(KC_NO),
-        KC_LGUI,            KC_TRNS,        LGUI(KC_X),       LGUI(KC_C),   LGUI(KC_V),
+        KC_LGUI,            KC_NO,          LGUI(KC_X),       LGUI(KC_C),   LGUI(KC_V),
                                                                                               KC_LALT, KC_LGUI,
                                                                                                        KC_HOME,
                                                                                       KC_SPC, KC_TAB,  KC_END,
         // right hand
-             LT(NPAD,KC_TRNS),  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_AUDIO_VOL_UP,
-             KC_TRNS,           KC_Z,   KC_U,  KC_I,   KC_O,   KC_P,             KC_TRNS,
-                                KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_TRNS),LT(SYMB, KC_TRNS),
-             MEH_T(KC_NO),      KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             LT(NPAD,KC_NO),  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_AUDIO_VOL_UP,
+             KC_NO,           KC_Z,   KC_U,  KC_I,   KC_O,   KC_P,             KC_NO,
+                              KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_NO),  LT(SYMB, KC_NO),
+             MEH_T(KC_NO),    KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                         LGUI(KC_Z), KC_UP,KC_DOWN, KC_LEFT, KC_RIGHT,
              KC_RGUI,        KC_RALT,
-             KC_TRNS,
-             KC_TRNS,KC_TAB, KC_ENT
+             KC_NO,
+             KC_NO,KC_TAB, KC_ENT
     ),
 
 
 [SYMB] = LAYOUT_ergodox(
        // left hand
-       VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       RESET,  KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       EPRM,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-          KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       VRSN,   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_NO,
+       RESET,  KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_NO,
+       KC_NO,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
+       EPRM,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_NO,
+          KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,
                                        RGB_MOD,RGB_LYR,
-                                               KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+                                               KC_NO,
+                               RGB_VAD,RGB_VAI,KC_NO,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       KC_NO, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_NO, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
+                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_NO,
+       KC_NO, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_NO,
+                         KC_NO,KC_DOT,  KC_0,    KC_EQL,  KC_NO,
        RGB_TOG, RGB_SLD,
-       KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       KC_NO,
+       KC_NO, RGB_HUD, RGB_HUI
 ),
 
 [MDIA] = LAYOUT_ergodox(
     // left hand
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_NO,   KC_MS_U,  KC_NO,    KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_MS_L, KC_MS_D,  KC_MS_R,  KC_NO,
+       KC_NO, KC_NO, KC_NO,   KC_NO,    KC_NO,    KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_NO,   KC_BTN1,  KC_BTN2,
+                                                  KC_NO,  KC_NO,
+                                                          KC_NO,
+                                            KC_NO, KC_NO, KC_NO,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY,
+       KC_NO,  KC_NO, KC_NO, KC_MPRV, KC_MNXT, KC_NO, KC_NO,
+                          KC_VOLU, KC_VOLD, KC_MUTE, KC_NO, KC_NO,
+       KC_NO, KC_NO,
+       KC_NO,
+       KC_NO, KC_NO, KC_NO
 ),
 
 [NPAD] = LAYOUT_ergodox(
     // left hand
-       RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_TRNS,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+       RESET, KC_NO, KC_NO, KC_NO, KC_NO,KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_7,  KC_8,  KC_9, KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_4,  KC_5,  KC_6, KC_NO,
+       KC_NO, KC_NO, KC_1,  KC_2,  KC_3, KC_NO, KC_NO,
+       KC_NO, KC_NO, KC_NO, KC_0,  KC_NO,
+                                           KC_NO, KC_NO,
+                                                  KC_NO,
+                                    KC_NO, KC_NO, KC_NO,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_7,    KC_8,    KC_9,    KC_TRNS, KC_TRNS,
-                 KC_TRNS, KC_4,    KC_5,    KC_6,    KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS, KC_TRNS,
-                          KC_TRNS, KC_0,    KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+       KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+       KC_NO,  KC_NO, KC_7,    KC_8,    KC_9,    KC_NO, KC_NO,
+               KC_NO, KC_4,    KC_5,    KC_6,    KC_NO, KC_NO,
+       KC_NO,  KC_NO, KC_1,    KC_2,    KC_3,    KC_NO, KC_NO,
+                      KC_NO,   KC_0,    KC_NO,   KC_NO, KC_NO,
+       KC_NO, KC_NO,
+       KC_NO,
+       KC_NO, KC_NO, KC_NO
 ),
 };
 
@@ -122,7 +122,6 @@ void eeconfig_init_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    // dynamically generate these.
     case EPRM:
       if (record->event.pressed) {
         eeconfig_init();
@@ -143,20 +142,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-     case RGB_LYR:  // This allows me to use underglow as layer indication, or as normal
+     case RGB_LYR:
         if (record->event.pressed) {
-            user_config.rgb_layer_change ^= 1; // Toggles the status
-            eeconfig_update_user(user_config.raw); // Writes the new status to EEPROM
-            if (user_config.rgb_layer_change) { // if layer state indication is enabled,
-                layer_state_set(layer_state);   // then immediately update the layer color
+            user_config.rgb_layer_change ^= 1;
+            eeconfig_update_user(user_config.raw);
+            if (user_config.rgb_layer_change) {
+                layer_state_set(layer_state);
             }
         }
         return false; break;
-    case RGB_MODE_FORWARD ... RGB_MODE_GRADIENT: // For any of the RGB codes (see quantum_keycodes.h, L400 for reference)
-        if (record->event.pressed) { //This disables layer indication, as it's assumed that if you're changing this ... you want that disabled
-            if (user_config.rgb_layer_change) {        // only if this is enabled
-                user_config.rgb_layer_change = false;  // disable it, and
-                eeconfig_update_user(user_config.raw); // write the setings to EEPROM
+    case RGB_MODE_FORWARD ... RGB_MODE_GRADIENT:
+        if (record->event.pressed) {
+            if (user_config.rgb_layer_change) {
+                user_config.rgb_layer_change = false;
+                eeconfig_update_user(user_config.raw);
             }
         }
         return true; break;
@@ -165,12 +164,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void matrix_init_user(void) {
-  // Call the keymap level matrix init.
-
-  // Read the user config from EEPROM
   user_config.raw = eeconfig_read_user();
 
-  // Set default layer, if enabled
   if (user_config.rgb_layer_change) {
     rgblight_enable_noeeprom();
     rgblight_sethsv_noeeprom_cyan();
@@ -178,7 +173,6 @@ void matrix_init_user(void) {
   }
 }
 
-// Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 
 };
@@ -222,7 +216,7 @@ uint32_t layer_state_set_user(uint32_t state) {
         ergodox_right_led_3_on();
         if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_white(); rgblight_mode_noeeprom(1); }
         break;
-    default: //  for any other layers, or the default layer
+    default:
         if (user_config.rgb_layer_change) { rgblight_sethsv_noeeprom_cyan(); rgblight_mode_noeeprom(1); }
         break;
     }
