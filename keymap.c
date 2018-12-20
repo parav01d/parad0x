d755679b8488c1e4c15a55208b509a376c28ebaf
Tom Kunzemann <tom.kunzemann@smartplatz.de>
2018-12-20 21:02:15 +0100

    finish right side first layer

--- keymap.c
+++ keymap.c
@@ -30,20 +30,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_AUDIO_VOL_DOWN,  KC_1,           KC_2,             KC_3,         KC_4,     KC_5,   LT(NPAD,KC_TRNS),
         KC_DELT,            KC_Q,           KC_W,             KC_E,         KC_R,     KC_T,   KC_TRNS,
         LT(SYMB,KC_BSPC),   LT(MDIA, KC_A), KC_S,             KC_D,         KC_F,     KC_G,
-        KC_LSFT,            CTL_T(KC_Z),    KC_X,             KC_C,         KC_V,     KC_B,   ALL_T(KC_NO),
+        KC_LSFT,            CTL_T(KC_Y),    KC_X,             KC_C,         KC_V,     KC_B,   ALL_T(KC_NO),
         KC_LGUI,            KC_TRNS,        LGUI(KC_X),       LGUI(KC_C),   LGUI(KC_V),
                                                                                               KC_LALT, KC_LGUI,
                                                                                                        KC_HOME,
                                                                                       KC_SPC, KC_TAB,  KC_END,
         // right hand
              LT(NPAD,KC_TRNS),  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_AUDIO_VOL_UP,
-             KC_TRNS,           KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                                KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
+             KC_TRNS,           KC_Z,   KC_U,  KC_I,   KC_O,   KC_P,             KC_TRNS,
+                                KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_TRNS),LT(SYMB, KC_TRNS),
              MEH_T(KC_NO),      KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                        KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          TT(SYMB),
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+                                        LGUI(KC_Z), KC_UP,KC_DOWN, KC_LEFT, KC_RIGHT,
+             KC_RGUI,        KC_RALT,
+             KC_TRNS,
+             KC_TRNS,KC_TAB, KC_ENT
     ),
 
 
